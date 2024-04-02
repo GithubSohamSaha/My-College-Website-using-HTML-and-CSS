593a7592d1a49b0d07783d022cf50a6c42c4ba21
Soham Saha <144131050+GithubSohamSaha@users.noreply.github.com>
2024-04-02 16:56:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+{/* <script>
+  document.getElementById("contact-us-link").addEventListener("click", function() {
+    window.scroll({
+      top: document.getElementById("footer-section").offsetTop,
+      behavior: "smooth"
+    });
+  });
+</script>
+ */}
